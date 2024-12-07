7be2226a2f95b46a0faa029988cf98b195ead8c3
dunzae <dunzae99@gmail.com>
2024-12-07 20:20:58 +0900

    @Feat zustand에서 persist를 사용했을 때, 값을 가져오는 동안 Loading 되는 상태를 표시

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ import { useAuthStore } from "./zustand/providers/auth";
 export default function Home() {
   const isLogin = useAuthStore((state) => state.login);
 
-  if(isLogin) {
+  if(isLogin === undefined) {
+    return <div>Loading</div>
+  }
+
+  if(isLogin === true) {
     return (<div>HomePage</div>);
   } 
 
